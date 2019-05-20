7b55cec0c867cbbb456c2c7b685393f660739fd0
fimion <fimion@gmail.com>
2019-05-20 07:47:27 -0400

    Fix format string typo

--- commands/hello.py
+++ commands/hello.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 class Greeting(commands.Cog):
     @commands.command()
     async def hello(self, ctx):
-        await ctx.send('Hello {0.display_name'.format(ctx.author))
+        await ctx.send('Hello {0.display_name}'.format(ctx.author))
 
 
 def setup(bot):
